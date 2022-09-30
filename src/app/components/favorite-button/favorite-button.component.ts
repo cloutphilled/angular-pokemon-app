f6f9ac9051459c7f83b988e9076173fa990e764a
Phillip Friis Petersen <phillipfriispetersen@gmail.com>
2022-09-30 10:48:47 +0200

    addtofavorites

--- src/app/components/favorite-button/favorite-button.component.ts
+++ src/app/components/favorite-button/favorite-button.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, Input, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user.model';
+import { FavoriteService } from 'src/app/services/favorite.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-favorite-button',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavoriteButtonComponent implements OnInit {
 
-  constructor() { }
+  public loading: boolean = false;
+  public isFavorite: boolean = false;
+  @Input() pokemonId: number = 0;
+
+  constructor(
+    private userService: UserService,
+    private readonly favoriteService: FavoriteService
+  ) { }
 
   ngOnInit(): void {
+    this.isFavorite = this.userService.inFavorites(this.pokemonId)
   }
 
-}
+  onFavoriteClick(): void {
+    this.loading = true;
+    this.favoriteService.addToFavorites(this.pokemonId)
+    .subscribe({
+      next: (user: User) => {
+        this.loading = false;
+        this.isFavorite = this.userService.inFavorites(this.pokemonId);
+      },
+      error: (error: HttpErrorResponse) => {
+        console.log('ERROR', error.message);
+      }
+    })
+    }
+  }
+
+
--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,9 +1,58 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { finalize } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Pokemon } from '../models/pokemon.models';
 
+
+const { apiPokemons } = environment;
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonService {
 
-  constructor() { }
+  private _pokemons: Pokemon[] = [];
+  private _error: string = "";
+  private _loading: boolean = false;
+
+  get pokemons(): Pokemon[] {
+    return this._pokemons;
+  }
+
+  get error(): string {
+    return this._error;
+  }
+
+  get loading(): boolean {
+    return this._loading;
+  }
+  
+
+  constructor(private readonly http: HttpClient) { }
+
+  public findAllPokemons(): void {
+    if(this._pokemons.length > 0 || this.loading) {
+      return;
+    }
+
+    this._loading = true;
+    this.http.get<Pokemon[]>(apiPokemons)
+    .pipe(
+      finalize(() => {
+        this._loading = false;
+      })
+    ).subscribe({
+      next: (pokemons: Pokemon[]) => {
+        this._pokemons = pokemons;
+      },
+      error: (error: HttpErrorResponse) => {
+        this._error = error.message;
+      }
+    })
+  }
+
+
+  public pokemonById(id: number): Pokemon | undefined {
+    return this._pokemons.find((pokemon: Pokemon) => pokemon.id === id);
+  }
 }
