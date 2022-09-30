93af07de19d7a5df7c8551ee72449f1bf2957741
VRosendue <110988416+VRosendue@users.noreply.github.com>
2022-09-30 09:53:14 +0200

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from "@angular/common/http"
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { LoginPage } from './pages/login/login.page';
+import { PokemonsPage } from './pages/pokemons/pokemons.page';
+import { ProfilePage } from './pages/profile/profile.page';
+import { LoginFormComponent } from './components/login-form/login-form.component';
+import { PokemonsListComponent } from './components/pokemons-list/pokemons-list.component';
+import { PokemonsListItemComponent } from './components/pokemons-list-item/pokemons-list-item.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FormsModule } from '@angular/forms';
+
+//Decorator
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginPage,
+    PokemonsPage,
+    PokemonsListComponent,
+    ProfilePage,
+    LoginFormComponent,
+    PokemonsListItemComponent,
+    NavbarComponent,
+    
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
